e473ac08106f2a4aecd72c7e2cf7bd613e5e8671
oskcarsv <oskcarqsv@gmail.com>
2024-05-05 14:31:52 -0600

    nav bar structure done

---
+++ src/components/task/taskWelcome.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { useState } from "react";
+
+const TaskWelcome = () => {
+  const username = localStorage.getItem("username");
+
+  return (
+    <div className="task-photo-profile">
+      <h1 className="task-title">Welcome 👋!</h1>
+      <h2>{username}</h2>
+    </div>
+  );
+};
+
+export { TaskWelcome };
