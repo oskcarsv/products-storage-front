ca43362ecccd71c3bc3340586be2cff8ca088bf4
oskcarsv <oskcarqsv@gmail.com>
2024-05-04 22:37:59 -0600

    modify some properties

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import { MainPage } from "./pages/main/mainPage"
 import { AuthPage } from "./pages/auth/AuthPage";
 import { TaskPage } from './pages/task/taskPage';
 
+// eslint-disable-next-line react-hooks/rules-of-hooks
 const routes = useRoutes([
     {path: '/*', element: <MainPage/>},
     {path: 'auth', element: <AuthPage/>},
--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import axios from "axios";
 import { logout } from "../shared/hooks";
 
