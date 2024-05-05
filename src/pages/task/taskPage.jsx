1ee49c172a2730c3aba90b42108fb090d6480490
oskcarsv <oskcarqsv@gmail.com>
2024-05-05 13:19:27 -0600

    create components structure

--- src/pages/task/taskPage.jsx
+++ src/pages/task/taskPage.jsx
@@ -5,12 +5,32 @@ import "./task.css";
 
 export const TaskPage = () => {
   return (
-    <div className="task-container">
-      <div className="">
 
-      </div>
+    <div className="task-container">
+      {/* Head */}
+      <nav className="task-navbar">
+          <h1>Task</h1>  
+      </nav>  
+
+      {/* Body */}
+      <article className="task-content">
+
+        {/* content */}
+        <section className="task-list">
+
+        </section>
+
+        <section className="task-view">
+
+          <div className="task-progress">
+          </div>
+
+          <div className="task-create">
+          </div>
+
+        </section>
+
+      </article>
     </div>
   );
 };
-
-
