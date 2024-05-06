b743cb82cfc5c391b76696c39e076f2357f53bf6
amendez2019362 <amendez-2019362@kinal.edu.gt>
2024-05-06 08:11:54 -0600

    new structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
-import {
-  BrowserRouter as Router,
-  useRoutes,
-} from 'react-router-dom';
-import { MainPage } from "./pages/main/mainPage"
-import { TaskPage } from './pages/task/taskPage';
+import { useState } from 'react';
+import './App.css';
+import { TodoAdd } from './components/TodoAdd';
+import { TodoList } from './components/TodoList';
+import { useTodo } from './hooks/useTodo';
 
-
-function AppRoutes() {
-  const routes = useRoutes(
-    [
-      { path: '/', element: <MainPage /> },
-      { path: '/task', element: <TaskPage /> },
-    ]
-  )
-  return routes;
-}
 function App() {
-  return (
-    <Router>
-      <AppRoutes />
-    </Router>
-  )
+	const {
+		todos,
+		todosCount,
+		pendingTodosCount,
+		handleNewTodo,
+		handleDeleteTodo,
+		handleCompleteTodo,
+		handleUpdateTodo,
+	} = useTodo();
+
+	return (
+		<>
+			<div className='card-to-do'>
+				<h1>Almacenadora</h1>
+				<div className='counter-todos'>
+					<h3>
+						N° Tareas: <span>{todosCount}</span>
+					</h3>
+					<h3>
+						Pendientes: <span>{pendingTodosCount}</span>
+					</h3>
+				</div>
+
+				<div className='add-todo'>
+					<h3>Agregar Tarea</h3>
+
+					<TodoAdd handleNewTodo={handleNewTodo} />
+				</div>
+
+				<TodoList
+					todos={todos}
+					handleUpdateTodo={handleUpdateTodo}
+					handleDeleteTodo={handleDeleteTodo}
+					handleCompleteTodo={handleCompleteTodo}
+				/>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/pages/task/task.js
+++
@@ -1,2 +0,0 @@
-export { taskPage } from './taskPage.jsx'
-
