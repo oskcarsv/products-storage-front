d707be1d6104e96675b5e735736bd80cadc43739
oskcarsv <oskcarqsv@gmail.com>
2024-05-06 03:44:07 -0600

    finish contentPage.jsx

--- src/components/dashboard/Content.jsx
+++ src/components/dashboard/Content.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Content.css";
 
 import todoIcon from "../../assets/taskImg/todo-icon.png";
@@ -24,38 +25,36 @@ export const Content = () => {
         <div className="welcome-text">
           <h1>Welcome to Products Storage</h1>
           <p className="p-welcome">
-            Products Storage is a simple contact management system that allows
-            you to manage your contacts with ease. You can add, edit, delete and
-            view your contacts.
+            Products Storage is a simple todo-list system that allows you to
+            manage your tasks ease. You can add, edit, delete and view your
+            tasks.
           </p>
 
           <div className="login">
-            <button>start </button>
+            <Link to="/task">Start</Link>
           </div>
         </div>
         <div className="welcome-img">
-          <img
-            src={welcomeImg}
-            alt="welcome"
-            height="400px"
-            width="600px"
-          />
+          <img src={welcomeImg} alt="welcome" height="400px" width="600px" />
         </div>
       </section>
 
       <section className="s-todo-list">
-            <div className="container-todo">
-                <div className="todo-img">
-                    <img src={todoImg} alt="welcome" height="600px" width="700px" />
-                </div>
-                <div className="todo-text">
-                    <h1>Todo list</h1>
-                    <p className="p-todo">
-                      A to-do list on a webpage is a feature that allows users to create and manage a list of tasks. It typically consists of a text field where users can enter the task name, a checkbox to mark the task as completed.
-                      </p>
-                </div>    
-            </div>
-        </section>
+        <div className="container-todo">
+          <div className="todo-img">
+            <img src={todoImg} alt="welcome" height="600px" width="700px" />
+          </div>
+          <div className="todo-text">
+            <h1>Todo list</h1>
+            <p className="p-todo">
+              A to-do list on a webpage is a feature that allows users to create
+              and manage a list of tasks. It typically consists of a text field
+              where users can enter the task name, a checkbox to mark the task
+              as completed.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
